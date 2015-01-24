d8ed61d737614f2c24a34955dd978e68cd7caf06
Peter Duerr <duerrp@gmail.com>
2015-01-24 11:18:43 +0900

    Remove unicode_literals import to avoid problems.
    * Configobj seems to have a problem with the from __future__ import.

--- pyexperiment/experiment.py
+++ pyexperiment/experiment.py
@@ -7,7 +7,7 @@ Written by Peter Duerr.
 """
 
 from __future__ import print_function
-from __future__ import unicode_literals
+# from __future__ import unicode_literals
 from __future__ import division
 from __future__ import absolute_import
 
