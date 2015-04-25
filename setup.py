915631cc0428d979f99f0a4aed56f5aaf8606015
Peter Duerr <duerrp@gmail.com>
2015-04-25 10:53:10 +0900

    Add tags for supported python versions in setup.py.

--- setup.py
+++ setup.py
@@ -41,5 +41,12 @@ setup(
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
         "License :: OSI Approved :: MIT License",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
     ],
 )
