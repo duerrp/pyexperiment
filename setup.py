c24f56949ff47a3846462cd05b2a84cd087b3b6f
Peter Duerr <peter@peterduerr.net>
2015-04-25 13:02:12 +0900

    Bump test version...

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ elif os.path.exists('README.md'):
 
 setup(
     name="pyexperiment",
-    version="0.1.16.dev6",
+    version="0.1.16.dev7",
     author="Peter Duerr",
     author_email="duerrp@gmail.com",
     description="Run experiments with Python - quick and clean.",
