ba66f62a6e4881a0c0c9cfa4e9636b07f4cfa74b
Peter Duerr <peter@peterduerr.net>
2015-04-26 21:57:07 +0900

    Relax sphinx version requirement for readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -60,7 +60,7 @@ from pyexperiment import __version__
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-needs_sphinx = '1.3'
+# needs_sphinx = '1.3'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
