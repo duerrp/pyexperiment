957d29ac29f175c4226cb5f668fa0355a4f35ad1
Peter Duerr <duerrp@gmail.com>
2015-04-28 21:44:50 +0900

    Allow passing arguments to interactive embed call.

--- pyexperiment/utils/interactive.py
+++ pyexperiment/utils/interactive.py
@@ -9,18 +9,23 @@ from __future__ import division
 from __future__ import absolute_import
 
 
-def embed_interactive():
+def embed_interactive(**kwargs):
     """Embed an interactive terminal into a running python process
     """
     try:
         import IPython
         ipython_config = IPython.Config()
         ipython_config.TerminalInteractiveShell.confirm_exit = False
-        IPython.embed(config=ipython_config,
-                      banner1='')
+        if IPython.__version__ == '1.2.1':
+            IPython.embed(config=ipython_config,
+                          banner1='',
+                          user_ns=kwargs)
+        else:
+            IPython.embed(config=ipython_config,
+                          banner1='',
+                          local_ns=kwargs)
     except ImportError:
         import readline  # pylint: disable=unused-variable
         import code
-        variables = globals().copy()
-        variables.update(locals())
-        code.InteractiveConsole(variables).interact()
+
+        code.InteractiveConsole(kwargs).interact()
