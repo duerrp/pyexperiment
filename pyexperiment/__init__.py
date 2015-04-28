c73fe21d20899242378f30f0a4de772cc71bf34e
Peter Duerr <duerrp@gmail.com>
2015-04-29 00:19:12 +0900

    Bump version...

--- pyexperiment/__init__.py
+++ pyexperiment/__init__.py
@@ -1,6 +1,6 @@
 """The pyexperiment module - quick and clean experiments with Python.
 """
-__version__ = '0.2.3'
+__version__ = '0.2.4'
 
 from pyexperiment.utils.Singleton import SingletonIndirector
 from pyexperiment.utils.Singleton import InitializeableSingletonIndirector
