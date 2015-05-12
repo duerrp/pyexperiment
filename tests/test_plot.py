7b54e6c82c1b87466b5076fda75ce87e4fb96e29
Peter Duerr <duerrp@gmail.com>
2015-05-13 01:20:31 +0900

    Add future imports to plot tests

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -3,6 +3,10 @@
 Written by Peter Duerr
 
 """
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import division
+from __future__ import absolute_import
 
 import unittest
 
