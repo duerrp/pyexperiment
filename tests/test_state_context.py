9b97683f164acd39bbff65c86ae48f9e20ba3232
Peter Duerr <duerrp@gmail.com>
2015-09-13 22:34:39 +0900

    More tests for state context

--- tests/test_state_context.py
+++ tests/test_state_context.py
@@ -10,11 +10,13 @@ from __future__ import absolute_import
 import unittest
 import threading
 import multiprocessing
+from six import StringIO
 
 from pyexperiment import state
 from pyexperiment.state_context import substate_context
 from pyexperiment.state_context import thread_state_context
 from pyexperiment.state_context import processing_state_context
+from pyexperiment.utils.stdout_redirector import stdout_err_redirector
 
 
 class TestSubStateContext(unittest.TestCase):
@@ -63,6 +65,26 @@ class TestSubStateContext(unittest.TestCase):
             self.assertEqual(state['a.a'], 12)
             self.assertEqual(state['a.b'], 13)
 
+    def test_get_nonexisting(self):
+        """Test getting an item of the state that does not exist
+        """
+        with substate_context('test'):
+            self.assertRaises(KeyError, lambda: state['bla'])
+
+    def test_iterate(self):
+        """Test iterating over sub state
+        """
+        with substate_context('test'):
+            state['a'] = 1
+            state['b'] = 2
+            for elem in state:
+                if elem == 'a':
+                    self.assertEqual(state[elem], 1)
+                elif elem == 'b':
+                    self.assertEqual(state[elem], 2)
+                else:
+                    assert False
+
 
 class TestThreadStateContext(unittest.TestCase):
     """Test the thread_state_context
@@ -93,6 +115,52 @@ class TestThreadStateContext(unittest.TestCase):
             for i in range(len(threads)):
                 self.assertEqual(state[str(i)], i)
 
+    def test_delete_nonexisting(self):
+        """Test deleting non-existing sub-state in threads
+        """
+        with thread_state_context():
+            def worker():
+                """thread worker function"""
+                def dell():
+                    """Test function"""
+                    del state['foo']
+                self.assertRaises(KeyError, dell)
+
+            threads = []
+            for _ in range(20):
+                thread = threading.Thread(target=worker)
+                thread.start()
+                threads.append(thread)
+
+            for thread in threads:
+                thread.join()
+
+    def test_after_exception(self):
+        """Test setting, getting state after exception in threads
+        """
+        state['a'] = 1
+        buf_out = StringIO()
+        buf_err = StringIO()
+        try:
+            with stdout_err_redirector(buf_out, buf_err):
+                with thread_state_context():
+                    def worker():
+                        """thread worker function"""
+                        raise RuntimeError
+
+                    threads = []
+                    for _ in range(20):
+                        thread = threading.Thread(target=worker)
+                        thread.start()
+                        threads.append(thread)
+
+                    for thread in threads:
+                        thread.join()
+                    raise RuntimeError
+        except RuntimeError:
+            pass
+        self.assertEqual(state['a'], 1)
+
 
 def worker1(i):
     """Process worker function, needs to be defined at top level"""
@@ -113,6 +181,24 @@ def worker3(i):
     """Process worker function, needs to be defined at top level"""
     try:
         _ = state[str(i)]
+    except KeyError:
+        return True
+    return False
+
+
+def worker4():
+    """Process worker function, needs to be defined at top level"""
+    try:
+        state[[1, 2, 3]] = 12
+    except TypeError:
+        return True
+    return False
+
+
+def worker5():
+    """Process worker function, needs to be defined at top level"""
+    try:
+        del state['bla']
     except KeyError:
         return True
     return False
@@ -174,6 +260,51 @@ class TestProcessingStateContext(unittest.TestCase):
 
         for i in range(n_jobs):
             self.assertTrue(results[i].get())
+
+    def test_raises_on_setting(self):
+        """Test setting bad state in 4 processes
+        """
+        with processing_state_context():
+            n_jobs = 200
+            pool = multiprocessing.Pool(processes=4)
+            results = []
+            for _ in range(n_jobs):
+                results.append(pool.apply_async(worker4))
+
+            pool.close()
+            pool.join()
+
+        for i in range(n_jobs):
+            self.assertTrue(results[i].get())
+
+    def test_raises_on_deleting(self):
+        """Test deleting bad state in 4 processes
+        """
+        with processing_state_context():
+            n_jobs = 200
+            pool = multiprocessing.Pool(processes=4)
+            results = []
+            for _ in range(n_jobs):
+                results.append(pool.apply_async(worker5))
+
+            pool.close()
+            pool.join()
+
+        for i in range(n_jobs):
+            self.assertTrue(results[i].get())
+
+    def test_after_exception(self):
+        """Test deleting bad state in 4 processes
+        """
+        state['a'] = 12
+        try:
+            with processing_state_context():
+                raise RuntimeError
+
+        except RuntimeError:
+            pass
+
+        self.assertEqual(state['a'], 12)
 
 
 if __name__ == '__main__':
